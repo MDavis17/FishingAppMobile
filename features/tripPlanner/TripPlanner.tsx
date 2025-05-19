abe41d9d014d0d044bc6abac6ebcc1974a9d7a33
Max Davis <mdavis@trainheroic.com>
2025-05-19 16:38:32 -0600

    added basic navigation

---
+++ features/tripPlanner/TripPlanner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TripPlanner() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Trip Planning Screen</Text>
+    </View>
+  );
+}
