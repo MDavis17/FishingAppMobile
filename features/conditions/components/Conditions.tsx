59adae3afabc9f5fd9f03ce1ba2296257a8bd600
Max Davis <mdavis@trainheroic.com>
2026-06-02 12:23:53 -0600

    added temp card and graph

--- features/conditions/components/Conditions.tsx
+++ features/conditions/components/Conditions.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { StyleSheet, ScrollView, View } from "react-native";
-import ConditionsCard from "./ConditionsCard";
+import { useLocation } from "common/context/LocationContext";
+import useConditionsWeather from "../hooks/useConditionsWeather";
+import TemperatureCard from "./TemperatureCard";
 
 export default function Conditions() {
+  const { location } = useLocation();
+  const { weather, isLoading, error } = useConditionsWeather(location);
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.cardContainer}>
-        <ConditionsCard />
+        <TemperatureCard
+          weather={weather}
+          isLoading={isLoading}
+          error={error}
+        />
       </View>
     </ScrollView>
   );
